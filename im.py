fc3f913ebf8b6984487ff7e0ac1a36c048df9302
Som <SohamSalunke007@outlook.com>
2022-11-29 19:58:41 +0530

    Moved images to a separate file to declutter

---
+++ im.py
@@ -0,0 +1,20 @@
+import tkinter
+import tkinter.messagebox
+from tkinter import ttk
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+attphoto = Image.open('attendance.png')
+attphoto = attphoto.resize((252,46))
+
+
+photo = Image.open('download.png')
+realphoto = photo.resize((404,126))
+
+profile = Image.open('pfp.png')
+profile = profile.resize((126,126))
+
+results = Image.open('results.png')
+
+    
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter.messagebox
 from tkinter import ttk
 from tkinter import *
 from PIL import Image, ImageTk
+import im
+
 
 top = tkinter.Tk()
 store = tkinter.StringVar()
@@ -10,27 +12,24 @@ top.geometry('1920x1080')
 
 top.grid_columnconfigure(0, weight = 1)
 top.title("PESU")
-photo = Image.open('download.png')
-realphoto = photo.resize((404,126))
-finalphoto = ImageTk.PhotoImage(realphoto)
+
+finalphoto = ImageTk.PhotoImage(im.realphoto)
+
 
 
 quack = Label(image = finalphoto)
 quack.grid(column = 0, row = 0)
 
 
-profile = Image.open('pfp.png')
-realprofile = profile.resize((126,126))
-finalimg = ImageTk.PhotoImage(realprofile)
-#profilelabel = Label(image = finalimg)
-#profilelabel.grid(column = 1, row = 0)
 
-attendance = Image.open('attendance.png')
-attendance = attendance.resize((252,46))
-att = ImageTk.PhotoImage(attendance)
+finalimg = ImageTk.PhotoImage(im.profile)
 
-results = Image.open('results.png')
-res = ImageTk.PhotoImage(results)
+
+
+att = ImageTk.PhotoImage(im.attphoto)
+
+
+res = ImageTk.PhotoImage(im.results)
 
 
 
